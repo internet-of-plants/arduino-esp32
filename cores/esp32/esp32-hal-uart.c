81f27f49c8d50b01fff5280f97338d12d88f693f
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-02-17 11:06:03 +0100

    Update esp32-hal-uart.c

--- cores/esp32/esp32-hal-uart.c
+++ cores/esp32/esp32-hal-uart.c
@@ -73,7 +73,7 @@ static uart_t _uart_bus_array[] = {
 
 // solves issue https://github.com/espressif/arduino-esp32/issues/6032
 // baudrate must be multiplied when CPU Frequency is lower than APB 80MHz
-uint32_t _get_effective_baudrate(uint32_t baudrate)
+uint32_t _get_effective_baudrate(uint32_t baudrate) 
 {
     uint32_t Freq = getApbFrequency()/1000000;
     if (Freq < 80) {
@@ -143,7 +143,7 @@ static void uart_event_task(void *args)
 }
 
 
-bool uartIsDriverInstalled(uart_t* uart)
+bool uartIsDriverInstalled(uart_t* uart) 
 {
     if(uart == NULL) {
         return 0;
@@ -155,25 +155,33 @@ bool uartIsDriverInstalled(uart_t* uart)
     return false;
 }
 
-void uartSetPins(uart_t* uart, uint8_t rxPin, uint8_t txPin)
+// Valid pin UART_PIN_NO_CHANGE is defined to (-1)
+// Negative Pin Number will keep it unmodified, thus this function can set individual pins
+void uartSetPins(uart_t* uart, int8_t rxPin, int8_t txPin, int8_t ctsPin, int8_t rtsPin)
 {
-    if(uart == NULL || rxPin >= SOC_GPIO_PIN_COUNT || txPin >= SOC_GPIO_PIN_COUNT) {
+    if(uart == NULL) {
         return;
     }
     UART_MUTEX_LOCK();
-    ESP_ERROR_CHECK(uart_set_pin(uart->num, txPin, rxPin, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
-    UART_MUTEX_UNLOCK();
+    // IDF uart_set_pin() will issue necessary Error Message and take care of all GPIO Number validation.
+    uart_set_pin(uart->num, txPin, rxPin, ctsPin, rtsPin); 
+    UART_MUTEX_UNLOCK();  
+}
 
+// 
+void uartSetHwFlowCtrlMode(uart_t *uart, uint8_t mode, uint8_t threshold) {
+    if(uart == NULL) {
+        return;
+    }
+    // IDF will issue corresponding error message when mode or threshold are wrong and prevent crashing
+    // IDF will check (mode > HW_FLOWCTRL_CTS_RTS || threshold >= SOC_UART_FIFO_LEN)
+    uart_set_hw_flow_ctrl(uart->num, (uart_hw_flowcontrol_t) mode, threshold);
 }
 
 
 uart_t* uartBegin(uint8_t uart_nr, uint32_t baudrate, uint32_t config, int8_t rxPin, int8_t txPin, uint16_t queueLen, bool inverted, uint8_t rxfifo_full_thrhd)
 {
     if(uart_nr >= SOC_UART_NUM) {
-        return NULL;
-    }
-
-    if(rxPin == -1 && txPin == -1) {
         return NULL;
     }
 
@@ -208,10 +216,10 @@ uart_t* uartBegin(uint8_t uart_nr, uint32_t baudrate, uint32_t config, int8_t rx
     ESP_ERROR_CHECK(uart_param_config(uart_nr, &uart_config));
     ESP_ERROR_CHECK(uart_set_pin(uart_nr, txPin, rxPin, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
 
-    // Is it right or the idea is to swap rx and tx pins?
+    // Is it right or the idea is to swap rx and tx pins? 
     if (inverted) {
         // invert signal for both Rx and Tx
-        ESP_ERROR_CHECK(uart_set_line_inverse(uart_nr, UART_SIGNAL_TXD_INV | UART_SIGNAL_RXD_INV));
+        ESP_ERROR_CHECK(uart_set_line_inverse(uart_nr, UART_SIGNAL_TXD_INV | UART_SIGNAL_RXD_INV));    
     }
 
     // Creating UART event Task
@@ -231,7 +239,7 @@ void uartEnd(uart_t* uart)
     if(uart == NULL) {
         return;
     }
-
+   
     UART_MUTEX_LOCK();
     uart_driver_delete(uart->num);
     if (uart->envent_task) {
@@ -248,13 +256,13 @@ void uartSetRxInvert(uart_t* uart, bool invert)
     if (uart == NULL)
         return;
 #if 0
-    // POTENTIAL ISSUE :: original code only set/reset rxd_inv bit
+    // POTENTIAL ISSUE :: original code only set/reset rxd_inv bit 
     // IDF or LL set/reset the whole inv_mask!
     if (invert)
         ESP_ERROR_CHECK(uart_set_line_inverse(uart->num, UART_SIGNAL_RXD_INV));
     else
         ESP_ERROR_CHECK(uart_set_line_inverse(uart->num, UART_SIGNAL_INV_DISABLE));
-
+    
 #else
     // this implementation is better over IDF API because it only affects RXD
     // this is supported in ESP32, ESP32-S2 and ESP32-C3
@@ -263,7 +271,7 @@ void uartSetRxInvert(uart_t* uart, bool invert)
         hw->conf0.rxd_inv = 1;
     else
         hw->conf0.rxd_inv = 0;
-#endif
+#endif 
 }
 
 
@@ -289,7 +297,7 @@ uint32_t uartAvailableForWrite(uart_t* uart)
         return 0;
     }
     UART_MUTEX_LOCK();
-    uint32_t available =  uart_ll_get_txfifo_len(UART_LL_GET_HW(uart->num));
+    uint32_t available =  uart_ll_get_txfifo_len(UART_LL_GET_HW(uart->num));  
     UART_MUTEX_UNLOCK();
     return available;
 }
@@ -373,7 +381,7 @@ void uartFlushTxOnly(uart_t* uart, bool txOnly)
     if(uart == NULL) {
         return;
     }
-
+    
     UART_MUTEX_LOCK();
     while(!uart_ll_is_tx_idle(UART_LL_GET_HW(uart->num)));
 
@@ -486,7 +494,7 @@ int log_printf(const char *format, ...)
         xSemaphoreTake(_uart_bus_array[s_uart_debug_nr].lock, portMAX_DELAY);
     }
 #endif
-
+    
     vsnprintf(temp, len+1, format, arg);
     ets_printf("%s", temp);
 
@@ -563,22 +571,22 @@ unsigned long uartBaudrateDetect(uart_t *uart, bool flg)
  * To start detection of baud rate with the uart the auto_baud.en bit needs to be cleared and set. The bit period is 
  * detected calling uartBadrateDetect(). The raw baudrate is computed using the UART_CLK_FREQ. The raw baudrate is 
  * rounded to the closed real baudrate.
- *
+ * 
  * ESP32-C3 reports wrong baud rate detection as shown below:
- *
+ * 
  * This will help in a future recall for the C3.
  * Baud Sent:          Baud Read:
  *  300        -->       19536
  * 2400        -->       19536
- * 4800        -->       19536
- * 9600        -->       28818
+ * 4800        -->       19536 
+ * 9600        -->       28818 
  * 19200       -->       57678
  * 38400       -->       115440
  * 57600       -->       173535
  * 115200      -->       347826
  * 230400      -->       701754
- *
- *
+ * 
+ * 
 */
 void uartStartDetectBaudrate(uart_t *uart) {
     if(uart == NULL) {
@@ -588,10 +596,10 @@ void uartStartDetectBaudrate(uart_t *uart) {
     uart_dev_t *hw = UART_LL_GET_HW(uart->num);
 
 #ifdef CONFIG_IDF_TARGET_ESP32C3
-
+    
     // ESP32-C3 requires further testing
-    // Baud rate detection returns wrong values
-
+    // Baud rate detection returns wrong values 
+   
     log_e("ESP32-C3 baud rate detection is not supported.");
     return;
 
@@ -607,7 +615,7 @@ void uartStartDetectBaudrate(uart_t *uart) {
     hw->auto_baud.en = 1;
 #endif
 }
-
+ 
 unsigned long
 uartDetectBaudrate(uart_t *uart)
 {
